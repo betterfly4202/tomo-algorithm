5eea5f45927f1d13e54b9804513b775f9d220152
Sangwon Cho <hyde0401@gmail.com>
2019-08-21 19:01:46 +0900

    Create Solution.java

---
+++ src/main/java/week_1/bryan/oddOccurrencesInArray/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] target) {
+        Arrays.sort(target);
+        int aloneNumber = -1;
+        for(int number : target){
+            if(aloneNumber == -1){
+                aloneNumber = number;
+                continue;
+            }
+            
+            if(aloneNumber == number)
+                aloneNumber = -1;
+            else
+                return aloneNumber;
+        }
+        return aloneNumber;
+    }
+}
