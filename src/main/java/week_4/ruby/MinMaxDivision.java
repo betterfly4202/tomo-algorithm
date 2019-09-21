92827e057212a68c8b65ff3941c451c0c2b6f32c
uunnaa <ckdbsk12@gmail.com>
2019-09-22 07:10:12 +0900

    ForgRiverOne 과제 제출 - O(N) - TC100%

---
+++ src/main/java/week_4/ruby/MinMaxDivision.java
@@ -0,0 +1,13 @@
+package week_4.ruby;
+
+/***
+ * https://app.codility.com/programmers/lessons/14-binary_search_algorithm/min_max_division/
+ * @author ruby
+ *
+ */
+public class MinMaxDivision {
+	public int solution(String S) {
+		
+		return 0;
+	}
+}
