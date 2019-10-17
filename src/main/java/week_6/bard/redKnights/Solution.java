f7c77af9ce21f5f0546bd4ba4c7872d85a9d9fc1
이충일 <betterfly4202@gmail.com>
2019-10-17 23:29:16 +0900

    redKnights

---
+++ src/main/java/week_6/bard/redKnights/Solution.java
@@ -0,0 +1,35 @@
+package week_6.bard.redKnights;
+
+import java.util.Scanner;
+
+/**
+ * Created by betterfly
+ * Date : 2019.10.20
+ */
+public class Solution {
+    static void printShortestPath(int n, int i_start, int j_start, int i_end, int j_end) {
+        // Print the distance along with the sequence of moves.
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] i_startJ_start = scanner.nextLine().split(" ");
+
+        int i_start = Integer.parseInt(i_startJ_start[0]);
+
+        int j_start = Integer.parseInt(i_startJ_start[1]);
+
+        int i_end = Integer.parseInt(i_startJ_start[2]);
+
+        int j_end = Integer.parseInt(i_startJ_start[3]);
+
+        printShortestPath(n, i_start, j_start, i_end, j_end);
+
+        scanner.close();
+    }
+}
