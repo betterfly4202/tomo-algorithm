a271db7a7903fd6839ed9baee78ad7deed51220f
이충일 <betterfly4202@gmail.com>
2019-11-03 18:53:04 +0900

    fault test case

--- src/main/java/week_8/brad/biggerIsGreater/Solution.java
+++ src/main/java/week_8/brad/biggerIsGreater/Solution.java
@@ -9,17 +9,33 @@ public class Solution {
             Character c = w.charAt(i);
             for(int j=i-1; j>=0; j--){
                 if(c > w.charAt(j)){
-                    return switchingCharacter(w, i, j);
+                    String result = switchingCharacter(w, i, j);
+                    return resortedCharacter(result, j+1);
                 }
             }
         }
         return "no answer";
     }
 
+    // 1차 정렬
     public static String switchingCharacter(String w, int i, int j){
         StringBuilder sb = new StringBuilder(w);
         sb.deleteCharAt(i);
         sb.insert(j, w.charAt(i));
         return sb.toString();
+    }
+
+    // 2차 정렬 > index 뒤로 제일 작은 값을 앞으로 뺴기
+    public static String resortedCharacter(String w, int idx){
+        for(int i = w.length()-1; i>idx; i--){
+            Character c = w.charAt(i);
+            for(int j=idx; j<i; j++){
+                if(c < w.charAt(j)){
+                    return switchingCharacter(w, i, j);
+                }
+            }
+        }
+
+        return w;
     }
 }
--- src/test/java/week_8/brad/biggerIsGreater/SolutionTest.java
+++ src/test/java/week_8/brad/biggerIsGreater/SolutionTest.java
@@ -14,20 +14,19 @@ public class SolutionTest {
 
     @Test
     public void 값찾기(){
-//        assertThat(Solution.biggerIsGreater("abdc"), is("acbd"));
-//        assertThat(Solution.biggerIsGreater("fedcbabcd"), is("fedcbabdc"));
-//        assertThat(Solution.biggerIsGreater("lmno"), is("lmon"));
-//        assertThat(Solution.biggerIsGreater("dcba"), is("no answer"));
+        assertThat(Solution.biggerIsGreater("abdc"), is("acbd"));
+        assertThat(Solution.biggerIsGreater("fedcbabcd"), is("fedcbabdc"));
+        assertThat(Solution.biggerIsGreater("lmno"), is("lmon"));
+        assertThat(Solution.biggerIsGreater("dcba"), is("no answer"));
 
 
-//        assertThat(Solution.biggerIsGreater("ab"), is("ba"));
-//        assertThat(Solution.biggerIsGreater("bb"), is("no answer"));
-//        assertThat(Solution.biggerIsGreater("hefg"), is("hegf"));
-//        assertThat(Solution.biggerIsGreater("dhck"), is("dhkc"));
-
-        // 내 답은 :                                       hdkc
+        assertThat(Solution.biggerIsGreater("ab"), is("ba"));
+        assertThat(Solution.biggerIsGreater("bb"), is("no answer"));
+        assertThat(Solution.biggerIsGreater("hefg"), is("hegf"));
+        assertThat(Solution.biggerIsGreater("dhck"), is("dhkc"));
         assertThat(Solution.biggerIsGreater("dkhc"), is("hcdk"));
-        // 옮기고 나서 뒤에 것 중 정렬해야함 > 낮은게 제일 앞으로
+
+        assertThat(Solution.biggerIsGreater("zyyyvvvvvuttsrqqokjjheedccb"), is("no answer"));
     }
 
     @Test
